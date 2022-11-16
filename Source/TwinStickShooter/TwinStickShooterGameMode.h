13a9527ac72ac7a2d25388cef796317f8609c2a6
r.giner <remi.giner@hotmail.fr>
2022-11-16 15:00:18 +0100

    [master] Initial commit

---
+++ Source/TwinStickShooter/TwinStickShooterGameMode.h
@@ -0,0 +1,20 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TwinStickShooterGameMode.generated.h"
+
+
+UCLASS(minimalapi)
+class ATwinStickShooterGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	ATwinStickShooterGameMode();
+
+	virtual AActor* ChoosePlayerStart_Implementation(AController* Player) override;
+};
+
+
+
