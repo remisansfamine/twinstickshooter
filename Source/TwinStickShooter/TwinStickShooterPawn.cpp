54d46ae26cd7ce5e9e97085674a992aa88ea3c29
Rémi GINER <r.giner@student.isartdigital.com>
2022-11-16 15:00:18 +0100

    [master] Initial commit

---
+++ Source/TwinStickShooter/TwinStickShooterPawn.cpp
@@ -0,0 +1,181 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#include "TwinStickShooterPawn.h"
+#include "TwinStickShooter.h"
+#include "TwinStickShooterProjectile.h"
+#include "TimerManager.h"
+#include <Runtime/Engine/Classes/Engine/Engine.h>
+
+#define print(text) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 5, FColor::White,text)
+
+const FName ATwinStickShooterPawn::MoveForwardBinding("MoveForward");
+const FName ATwinStickShooterPawn::MoveRightBinding("MoveRight");
+const FName ATwinStickShooterPawn::FireForwardBinding("FireForward");
+const FName ATwinStickShooterPawn::FireRightBinding("FireRight");
+
+ATwinStickShooterPawn::ATwinStickShooterPawn()
+{	
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> ShipMesh(TEXT("/Game/TwinStick/Meshes/TwinStickUFO.TwinStickUFO"));
+	// Create the mesh component
+	ShipMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ShipMesh"));
+	RootComponent = ShipMeshComponent;
+	ShipMeshComponent->SetCollisionProfileName(UCollisionProfile::Pawn_ProfileName);
+	ShipMeshComponent->SetStaticMesh(ShipMesh.Object);
+	
+	// Cache our sound effect
+	static ConstructorHelpers::FObjectFinder<USoundBase> FireAudio(TEXT("/Game/TwinStick/Audio/TwinStickFire.TwinStickFire"));
+	FireSound = FireAudio.Object;
+
+	// Create a camera boom...
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->SetAbsolute(false, true, false); // Don't want arm to rotate when ship does
+	CameraBoom->TargetArmLength = 1200.f;
+	CameraBoom->SetRelativeRotation(FRotator(-80.f, 0.f, 0.f));
+	CameraBoom->bDoCollisionTest = false; // Don't want to pull camera in when it collides with level
+
+	// Create a camera...
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("TopDownCamera"));
+	CameraComponent->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	CameraComponent->bUsePawnControlRotation = false;	// Camera does not rotate relative to arm
+
+	// Movement
+	MoveSpeed = 1000.0f;
+	// Weapon
+	GunOffset = FVector(90.f, 0.f, 0.f);
+	FireRate = 0.1f;
+	bCanFire = true;
+	bIsAlive = true;
+
+	CurrentHealthPoints = MaxHealthPoints;
+
+	SetReplicates(true);
+}
+
+void ATwinStickShooterPawn::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	check(PlayerInputComponent);
+
+	// set up gameplay key bindings
+	PlayerInputComponent->BindAxis(MoveForwardBinding);
+	PlayerInputComponent->BindAxis(MoveRightBinding);
+	PlayerInputComponent->BindAxis(FireForwardBinding);
+	PlayerInputComponent->BindAxis(FireRightBinding);
+}
+
+void ATwinStickShooterPawn::Tick(float DeltaSeconds)
+{
+	if (!bIsAlive)
+		return;
+
+	// Find movement direction
+	ForwardValue = GetInputAxisValue(MoveForwardBinding);
+	RightValue = GetInputAxisValue(MoveRightBinding);
+
+	// Clamp max size so that (X=1, Y=1) doesn't cause faster movement in diagonal directions
+	MoveDirection = FVector(ForwardValue, RightValue, 0.f).GetClampedToMaxSize(1.0f);
+
+	// Create fire direction vector
+	const float FireForwardValue = GetInputAxisValue(FireForwardBinding);
+	const float FireRightValue = GetInputAxisValue(FireRightBinding);
+	const FVector FireDirection = FVector(FireForwardValue, FireRightValue, 0.f);
+
+	// Try and fire a shot
+	FireShot(FireDirection);
+
+	// Apply movement
+	ComputeMove(DeltaSeconds);
+}
+
+void ATwinStickShooterPawn::ComputeMove(float DeltaSeconds)
+{
+	if (ForwardValue == 0.0f && RightValue == 0.0f)
+		return;
+
+	// Calculate  movement
+	const FVector Movement = MoveDirection * MoveSpeed * DeltaSeconds;
+
+	// If non-zero size, move this actor
+	if (Movement.SizeSquared() > 0.0f)
+	{
+		const FRotator NewRotation = Movement.Rotation();
+		FHitResult Hit(1.f);
+		RootComponent->MoveComponent(Movement, NewRotation, true, &Hit);
+
+		if (Hit.IsValidBlockingHit())
+		{
+			const FVector Normal2D = Hit.Normal.GetSafeNormal2D();
+			const FVector Deflection = FVector::VectorPlaneProject(Movement, Normal2D) * (1.f - Hit.Time);
+			RootComponent->MoveComponent(Deflection, NewRotation, true);
+		}
+	}
+}
+
+void ATwinStickShooterPawn::FireShot(FVector FireDirection)
+{
+	// If we it's ok to fire again
+	if (bCanFire == true)
+	{
+		// If we are pressing fire stick in a direction
+		if (FireDirection.SizeSquared() > 0.0f)
+		{
+			const FRotator FireRotation = FireDirection.Rotation();
+			// Spawn projectile at an offset from this pawn
+			const FVector SpawnLocation = GetActorLocation() + FireRotation.RotateVector(GunOffset);
+
+			UWorld* const World = GetWorld();
+			if (World != NULL)
+			{
+				// spawn the projectile
+				World->SpawnActor<ATwinStickShooterProjectile>(SpawnLocation, FireRotation);
+			}
+
+			bCanFire = false;
+			World->GetTimerManager().SetTimer(TimerHandle_ShotTimerExpired, this, &ATwinStickShooterPawn::ShotTimerExpired, FireRate);
+
+			// try and play the sound if specified
+			if (FireSound != nullptr)
+			{
+				UGameplayStatics::PlaySoundAtLocation(this, FireSound, GetActorLocation());
+			}
+
+			bCanFire = false;
+		}
+	}
+}
+
+void ATwinStickShooterPawn::ShotTimerExpired()
+{
+	bCanFire = true;
+}
+
+void ATwinStickShooterPawn::Revive()
+{
+	print("Reviving Player !");
+	bIsAlive = true;
+	CurrentHealthPoints = MaxHealthPoints;
+	ShipMeshComponent->SetVisibility(true);
+	ShipMeshComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+}
+
+float ATwinStickShooterPawn::TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser)
+{
+	const float ActualDamage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+
+	if (ActualDamage > 0.f && bIsAlive)
+	{
+		CurrentHealthPoints -= ActualDamage;
+		if (CurrentHealthPoints <= 0)
+		{
+			print("Player is dead");
+			bIsAlive = false;
+			CurrentHealthPoints = 0;
+			ShipMeshComponent->SetVisibility(false);
+			ShipMeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+			//K2_DestroyActor();
+			// "revive" player after a delay
+			GetWorld()->GetTimerManager().SetTimer(TimerHandle_Revive, this, &ATwinStickShooterPawn::Revive, ReviveDelay, false);
+		}
+	}
+	return ActualDamage;
+}
---
+++ Source/TwinStickShooter/TwinStickShooterPawn.h
@@ -0,0 +1,105 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "TwinStickShooterPawn.generated.h"
+
+UCLASS(Blueprintable)
+class ATwinStickShooterPawn : public APawn
+{
+	GENERATED_BODY()
+
+	/* The mesh component */
+	UPROPERTY(Category = Mesh, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* ShipMeshComponent;
+
+	/** The camera */
+	UPROPERTY(Category = Camera, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* CameraComponent;
+
+	/** Camera boom positioning the camera above the character */
+	UPROPERTY(Category = Camera, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+public:
+	ATwinStickShooterPawn();
+
+	/** Offset from the ships location to spawn projectiles */
+	UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite )
+	FVector GunOffset;
+	
+	/* How fast the weapon will fire */
+	UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
+	float FireRate;
+
+	/* The speed our ship moves around the level */
+	UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
+	float MoveSpeed;
+
+	UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
+	int MaxHealthPoints = 10;
+
+	UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
+	float ReviveDelay = 2.0f;
+
+	/** Sound to play each time we fire */
+	UPROPERTY(Category = Audio, EditAnywhere, BlueprintReadWrite)
+	class USoundBase* FireSound;
+
+	// Begin Actor Interface
+	virtual void Tick(float DeltaSeconds) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+	// End Actor Interface
+
+	/* Handler for the fire timer expiry */
+	void ShotTimerExpired();
+
+	// Static names for axis bindings
+	static const FName MoveForwardBinding;
+	static const FName MoveRightBinding;
+	static const FName FireForwardBinding;
+	static const FName FireRightBinding;
+
+private:
+
+	/* Flag to control firing  */
+	uint32 bCanFire : 1;
+
+	/* Flag to set death state */
+	uint32 bIsAlive : 1;
+
+	/** Handle for efficient management of ShotTimerExpired timer */
+	FTimerHandle TimerHandle_ShotTimerExpired;
+
+	FTimerHandle TimerHandle_Revive;
+
+	UPROPERTY()
+	float ForwardValue;
+	UPROPERTY()
+	float RightValue;
+
+	UPROPERTY()
+	FVector MoveDirection;
+
+	int CurrentHealthPoints;
+
+	/* Fire a shot in the specified direction */
+	void FireShot(FVector FireDirection);
+
+	/* Compute Pawn movement */
+	void ComputeMove(float DeltaSeconds);
+
+	/* Revive Pawn */
+	void Revive();
+
+	float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
+
+public:
+	/** Returns ShipMeshComponent subobject **/
+	FORCEINLINE class UStaticMeshComponent* GetShipMeshComponent() const { return ShipMeshComponent; }
+	/** Returns CameraComponent subobject **/
+	FORCEINLINE class UCameraComponent* GetCameraComponent() const { return CameraComponent; }
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+};
+
