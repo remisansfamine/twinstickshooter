761702861fa3e44d3cbe4450b5014273b656bd6b
r.giner <remi.giner@hotmail.fr>
2022-11-28 12:12:23 +0100

    [pseudo] Added username replication

--- Source/TwinStickShooter/TwinStickShooterPlayerState.cpp
+++ Source/TwinStickShooter/TwinStickShooterPlayerState.cpp
@@ -2,11 +2,35 @@
 
 
 #include "TwinStickShooterPlayerState.h"
+
+#include "GameFramework/GameSession.h"
 #include "Net/UnrealNetwork.h"
+
+#include "TwinStickShooterInstance.h"
+
+void ATwinStickShooterPlayerState::BeginPlay()
+{
+	UpdateUsername();
+}
 
 void ATwinStickShooterPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 	DOREPLIFETIME(ATwinStickShooterPlayerState, DeathCount);
 	DOREPLIFETIME(ATwinStickShooterPlayerState, KillCount);
+	DOREPLIFETIME(ATwinStickShooterPlayerState, Username);
+}
+
+void ATwinStickShooterPlayerState::UpdateUsername()
+{
+	if (GetLocalRole() != ENetRole::ROLE_AutonomousProxy || GetRemoteRole() == ENetRole::ROLE_Authority)
+	{
+		if (UTwinStickShooterInstance* instance = Cast<UTwinStickShooterInstance>(GetGameInstance()))
+			SetUsername(instance->Username);
+	}
+}
+
+void ATwinStickShooterPlayerState::SetUsername_Implementation(const FString& NewUsername)
+{
+	Username = NewUsername;
 }
