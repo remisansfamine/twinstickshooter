deb36a344f2aed6019f661b246eda5171c7db905
r.giner <remi.giner@hotmail.fr>
2022-11-29 01:41:50 +0100

    [mvp] Started best player and highscore implementation

--- Source/TwinStickShooter/TwinStickShooterGameMode.cpp
+++ Source/TwinStickShooter/TwinStickShooterGameMode.cpp
@@ -5,12 +5,21 @@
 #include "TwinStickShooterPawn.h"
 #include "TwinStickShooterPlayerState.h"
 #include "GameFrameWork/PlayerStart.h"
+#include "Net/UnrealNetwork.h"
 
 ATwinStickShooterGameMode::ATwinStickShooterGameMode()
 {
 	// set default pawn class to our character class
 	DefaultPawnClass = ATwinStickShooterPawn::StaticClass();
 	PlayerStateClass = ATwinStickShooterPlayerState::StaticClass();
+
+}
+
+void ATwinStickShooterGameMode::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(ATwinStickShooterGameMode, BestPlayer);
+	DOREPLIFETIME(ATwinStickShooterGameMode, ScoreToReach);
 }
 
 AActor * ATwinStickShooterGameMode::ChoosePlayerStart_Implementation(AController * Player)
@@ -29,4 +38,22 @@ AActor * ATwinStickShooterGameMode::ChoosePlayerStart_Implementation(AController
 	}
 
 	return nullptr;
+}
+
+void ATwinStickShooterGameMode::UpdateBestScore(APlayerState* PlayerState)
+{
+	const float PlayerScore = PlayerState->GetScore();
+	if (!BestPlayer || PlayerScore > BestPlayer->GetScore())
+	{
+		BestPlayer = PlayerState;
+
+		if (OnBestPlayerChange.IsBound())
+			OnBestPlayerChange.Broadcast(BestPlayer);
+		
+		if (PlayerScore >= ScoreToReach)
+		{
+			if (OnWinReached.IsBound())
+				OnWinReached.Broadcast(BestPlayer);
+		}
+	}
 }
